f548b0c5528e929c3703740daaba14219d362be7
joob <46855393+joobah@users.noreply.github.com>
2025-03-02 00:50:54 -0800

    relu.s

--- project2/src/relu.s
+++ project2/src/relu.s
@@ -15,19 +15,22 @@
 relu:
     # Prologue
 
+    li t0, 1
+    blt a1, t0, error 
+
+    add t0, a0, zero          # array memory location  
+    li t1, 0                  # array index
 
 loop_start:
-
-
-
-
-
-
-
+    beq t1, a1 loop_end
+    lw t2, 0(t0)
+    bge t2, zero loop_continue
+    sw zero, 0(t0)
 
 loop_continue:
-
-
+    addi t0, t0, 4
+    addi t1, t1, 1
+    j loop_start
 
 loop_end:
 
@@ -36,3 +39,9 @@ loop_end:
 
 
     jr ra
+
+error:
+    li a0, 36
+    j exit
+
+    
