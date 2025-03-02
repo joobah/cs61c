d3152fb1eed00b554436526d4ed0d50df0dfa60d
joob <46855393+joobah@users.noreply.github.com>
2025-03-02 14:22:31 -0800

    dot.s

--- project2/src/dot.s
+++ project2/src/dot.s
@@ -21,24 +21,45 @@ dot:
 
     # Prologue
 
+    li t0, 1
+    blt a2, t0, error_36
+    blt a3, t0, error_37
+    blt a4, t0, error_37
+
+    li t0, 0                  # t0 = number of elements used so far
+    li t1, 4                  # t1 = word size
+    li t2, 0                  # running dot product
 
 loop_start:
+    beq t0, a2 loop_end
 
+    mul t3, t0, t1
+    mul t3, t3, a3
+    add t3, t3, a0
+    lw t3, 0(t3)
 
+    mul t4, t0, t1
+    mul t4, t4, a4
+    add t4, t4, a1
+    lw t4, 0(t4)
 
+    mul t3, t3, t4
+    add t2, t2, t3
 
-
-
-
-
-
-
-
+    addi t0, t0, 1
+    j loop_start
 
 loop_end:
 
-
     # Epilogue
 
-
+    add a0, t2, zero
     jr ra
+
+error_36:
+    li a0, 36
+    j exit
+
+error_37:
+    li a0, 37
+    j exit
