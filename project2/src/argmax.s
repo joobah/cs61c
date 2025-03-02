a15e1cb6390f2b722669dfdf4722ce8b5f44d729
joob <46855393+joobah@users.noreply.github.com>
2025-03-02 01:07:08 -0800

    argmax.s

--- project2/src/argmax.s
+++ project2/src/argmax.s
@@ -17,14 +17,32 @@
 argmax:
     # Prologue
 
+    li t0, 1
+    blt a1, t0, error 
+
+    addi t0, a0, 4            # current array memory location  
+    li t1, 1                  # current index
+    lw t2, 0(a0)              # current max
+    li t3, 0                  # current max index
 
 loop_start:
-
+    beq t1, a1 loop_end
+    lw t4, 0(t0)
+    ble t4, t2 loop_continue
+    add t2, t4, zero
+    add t3, t1 zero
 
 loop_continue:
-
+    addi t0, t0, 4
+    addi t1, t1, 1
+    j loop_start
 
 loop_end:
     # Epilogue
 
+    add a0, t3, zero
     jr ra
+
+error:
+    li a0, 36
+    j exit
