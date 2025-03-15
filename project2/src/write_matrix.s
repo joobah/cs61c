efa7cc7b54dfec314f2d512c18b4e034a177caa4
joob <46855393+joobah@users.noreply.github.com>
2025-03-15 14:41:24 -0700

    write_matrix.s

--- project2/src/write_matrix.s
+++ project2/src/write_matrix.s
@@ -25,16 +25,79 @@
 write_matrix:
 
     # Prologue
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
 
+    mv s0, a1 # matrix pointer
+    mv s1, a2 # num rows
+    mv s2, a3 # num cols
 
+    # open file 
+    li a1, 1
+    jal ra, fopen
+    bltz a0 fopen_error
 
+    mv s3, a0 # file descriptor
 
+    addi sp, sp, -4
 
+    # write rows
+    sw s1, 0(sp) # put num rows in stack
+    mv a1, sp
+    li a2, 1
+    li a3, 4
+    jal ra, fwrite
+    li t0, 1
+    bne a0, t0 fwrite_error
 
+    # write cols
+    mv a0, s3
+    sw s2, 0(sp) # put num cols in stack
+    mv a1, sp
+    li a2, 1
+    li a3, 4
+    jal ra, fwrite
+    li t0, 1
+    bne a0, t0 fwrite_error
 
+    addi sp, sp, 4
 
+    # write matrix
+    mv a0, s3
+    mv a1, s0
+    mul a2, s1, s2
+    li a3, 4
+    jal ra, fwrite
+    mul t0, s1, s2
+    bne a0, t0 fwrite_error
+
+    # close file
+    mv a0, s3
+    jal ra, fclose
+    bltz a0 fclose_error
 
     # Epilogue
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    addi sp, sp, 20
 
     jr ra
+
+fopen_error:
+    li a0, 27
+    j exit
+
+fclose_error:
+    li a0, 28
+    j exit
+
+fwrite_error:
+    li a0, 30
+    j exit
